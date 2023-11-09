e0b9209c2202f9ff54fb73994d81376a4ffba2e7
Alice(LebedevDV) <fixinit1121@gmail.com>
2023-11-09 18:40:32 +0300

    code: добавлен вывод максимума

--- project/main.cpp
+++ project/main.cpp
@@ -7,7 +7,13 @@ int main()
     cout << "Enter A and B: ";
 int a, b;
 cin >> a >> b;
-cout << " A + B = " << a + b << '\n'
-<< "A - B = " << a - b << '\n'
-<< "A / B = " << a / b << '\n';
+cout << "A + B = " << a + b << '\n'
+ << "A - B = " << a - b << '\n'
+ << "A * B = " << a * b << '\n'
+ << "A / B = " << a / b << '\n';
+ if (a > b)
+    cout << "max: " << a;
+if (b > a)
+    cout << "max: " << b;
+
 }
